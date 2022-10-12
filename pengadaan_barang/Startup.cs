2a9682249d3da6a061df2d14268efc0b802d1953
cinditrifs <Cinditrifs@gmail.com>
2022-10-12 21:33:30 +0700

    add landing page

--- pengadaan_barang/Startup.cs
+++ pengadaan_barang/Startup.cs
@@ -61,7 +61,7 @@ namespace Client
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Account}/{action=Login}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
